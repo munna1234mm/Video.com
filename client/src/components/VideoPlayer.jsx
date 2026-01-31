e2a5c03b068378009ad510b2e5ddbc8ed51a3e57
unknown <munna1234kkfterte@gmail.com>
2026-01-30 15:57:14 -1200

    Use Google Sample VAST tag for debugging

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -26,7 +26,7 @@ const VideoPlayer = ({ src, poster, title, vastTag }) => {
                 adList: [
                     {
                         roll: 'preRoll',
-                        vastTag: vastTag
+                        vastTag: "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=" // Google Test Tag
                     }
                 ],
                 skipButtonCaption: 'Skip ad in [seconds]',
