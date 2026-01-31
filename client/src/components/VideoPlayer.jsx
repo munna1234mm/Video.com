76531ff42be8e57efb96a47c3dabbe7f0317062f
unknown <munna1234kkfterte@gmail.com>
2026-01-30 16:41:56 -1200

    Update VAST tag to user's ad zone URL

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -25,7 +25,7 @@ const VideoPlayer = ({ src, poster, title, vastTag }) => {
                 adList: [
                     {
                         roll: 'preRoll',
-                        vastTag: "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator="
+                        vastTag: "https://quickwittedconclusion.com/dZm.FOzxd/GJNLvtZtGrUS/TeEm/9/uHZYU/ldkTP/TWYq3/NEjFEy2/NNTQMbt/NWjGcH2_M/T/Yt1HNJAw"
                     }
                 ],
                 skipButtonCaption: 'Skip ad in [seconds]',
